758478f4ab320513c88e1d10842385b5e0b3cb14
Mark Peters <mapeters101@earthlink.net>
2024-11-25 17:46:07 -0500

    Work on lesson to 6:30.

--- lesson005.cpp
+++ lesson005.cpp
@@ -12,6 +12,11 @@ void func2(int *y) {			// y will be a pointer to the callers's variable x
 	*y = 1;				
 }
 
+void func3(int& y) {			// y will be a reference to the callers's variable x
+	cout << "address of y is " << &y << endl;
+	y = 1;
+}
+
 
 int main() {
 	int x = 2;
@@ -25,6 +30,13 @@ int main() {
 	x = 2;
 	cout << "Address of x is " << &x << endl;
 	func2(&x);		// x will now have the value 1
+	cout << "After calling func2(), x = " << x << endl;
+
+	cout << endl << endl;
+
+	x = 2;
+	cout << "Address of x is " << &x << endl;
+	func3(x);		// x will now have the value 1
 	cout << "After calling func2(), x = " << x << endl;
 
 
