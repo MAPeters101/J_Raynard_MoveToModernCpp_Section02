a5b62efd4d36c329b0ee70d8b37a6a292d137816
Mark Peters <mapeters101@earthlink.net>
2024-11-25 17:32:10 -0500

    Work on lesson to 4:15.

--- lesson005.cpp
+++ lesson005.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int func(int y) {			// y will be a copy of the callers's variable x
 	cout << "address of y is " << &y << endl;
-	return y				// Copy y into the function's return value
+	return y;				// Copy y into the function's return value
 }
 
 int main() {
-
+	int x = 2;
+	cout << "Address of x is " << &x << endl;
+	int z = func(x);		// z will be a copy of func's return value
+	cout << "address of z is " << &z << endl;
+	cout << "After calling func(), z = " << z << endl;
 }
