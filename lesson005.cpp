01b1f128836c0b3e71e17de199eea180e33684cf
Mark Peters <mapeters101@earthlink.net>
2024-11-25 17:24:33 -0500

    Lesson005:  Enter partial code.

--- lesson005.cpp
+++ lesson005.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int func(int y) {			// y will be a copy of the callers's variable x
+	cout << "address of y is " << &y << endl;
+	return y				// Copy y into the function's return value
+}
+
 int main() {
 
 }
